84b359c6d33ee9029ec8b158cb72029cf29b88e6
Emanuele Mancari <emanuele.mancari@abstract.it>
2024-04-05 09:41:02 +0200

    init copy

---
+++ src/data/copy.js
@@ -0,0 +1,28 @@
+export const copy = {
+    "official_Startupper_URL": "https://dominio.com",
+    "official_Startupper_title": "NomeAgenzia",
+    "official_Startupper_description": "Break official benefit entire. Person I morning have do. Place family child inside admit production. Rather movement like cell later. Bring six daughter get far full. Easy book Mr happy sit. Accept tree then old. Test natural deal window these pressure. Like describe write someone. Give thought set American.",
+    "official_Startupper_slogan": "Lorem ipsum super tiger mama bruta",
+    "header": {
+        "title": "",
+        "subtitle_short": "",
+        "call_to_action": ""
+    },
+    "footer": {
+        "title": "",
+        "subtitle": "",
+        "call_to_action": ""
+    },
+    "contact": {
+        "phone1": "3456789876",
+        "phone2": "",
+        "wa1": "3456789876",
+        "wa2": "",
+        "mail1": "ifo@nomeagenzia.com",
+        "mail2": "Iscriviti",
+    },
+    "utils": {
+        "call_to_action": "GoGoGo",
+    }
+  };
+  
