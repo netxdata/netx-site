3547bb2759f673aa59c090dfbf056c8fda59501f
Emanuele Mancari <emanuele.mancari@abstract.it>
2024-04-12 08:53:47 +0200

    update data

--- src/data/data.js
+++ src/data/data.js
@@ -1,3 +1,181 @@
+
+const name = "Bridge-Tech Solutions"
+const cta_1 = "Contattaci"
+const cta_2 = "Servizi"
+export const emanuele_wa = "+393791595630"
+const antonio_wa = "+393791595630"
+
+export const identity = {
+    "name": name,
+    "category": "Soluzioni Software su Misura",
+    "description_full": name + " si distingue come uno dei leader nello sviluppo di soluzioni software su misura. Attraverso un percorso che va dalla progettazione all'implementazione di sistemi complessi, ci rivolgiamo a una vasta gamma di clienti, guidati sempre da un principio fondamentale: la personalizzazione. La nostra passione per l'innovazione tecnologica ci permette di offrire soluzioni eccellenti, pensate ad hoc per soddisfare ogni specifica necessità di sviluppo software. Il nostro vero punto di forza, però, risiede nella capacità di comprendere a fondo le esigenze delle startup. Grazie a relazioni consolidate nel settore e alla nostra esperienza diretta, siamo in grado di identificare e affrontare efficacemente le sfide tecnologiche uniche di ogni realtà emergente. Il nostro obiettivo è fornire un supporto concreto e su misura, per trasformare ogni idea innovativa in realtà tangibile.",
+    "description_short": name + ", leader in soluzioni software personalizzate, trasforma idee in realtà con innovazione e personalizzazione. Specializzati in sistemi complessi per vari clienti, comprendiamo profondamente le sfide delle startup, offrendo soluzioni tecnologiche su misura per ogni esigenza.",
+    "slogan": "Il ponte tra tecnologia e innovazione.",
+    "team": "Siamo un team affiatato alla " + name + ", con radici ben piantate nel mondo delle startup e una passione sfrenata per la tecnologia. Siamo più di colleghi: siamo amici, uniti da legami forti nel settore, pronti a fare quel miglio in più per far decollare le vostre idee. Per noi, ogni sfida tecnologica è un'avventura, e ogni startup con cui collaboriamo diventa parte della famiglia.",
+    "phone1": emanuele_wa,
+    "mail1": "laparteweb@gmail.com",
+    "services": [
+        "Consulenza Tecnica per Startup",
+        "Integrazione di Sistemi e Automazione dei Processi",
+        "Sviluppo di Software e Architetture Personalizzate",
+        "Analisi e Miglioramento della Sicurezza Informatica",
+        "Ottimizzazione delle Performance di Sistemi Esistenti"
+    ],
+    "servicesDescription": [
+        "Ti guidiamo nel mondo tech, aiutandoti a scegliere le soluzioni più smart per crescere rapidamente.",
+        "Semplifichiamo i tuoi processi, facendo parlare tra loro le diverse tecnologie per farti risparmiare tempo e risorse.",
+        "Costruiamo il cuore tecnologico del tuo progetto, realizzando software che non solo funziona ma ispira.",
+        "Ci prendiamo cura dei tuoi dati come fossero i nostri, blindando la tua attività contro le minacce digitali.",
+        "Mettiamo a punto le tue tecnologie esistenti, perché ogni secondo in meno che aspetti è un valore aggiunto per te."
+    ],      
+    "copyServices": "In " + name + ", accompagniamo le startup nel selezionare le migliori tecnologie e offriamo soluzioni avanzate per integrare e automatizzare i processi. Mettiamo la sicurezza al primo posto, con analisi approfondite per proteggere i tuoi dati e garantire che i sistemi siano sempre agili e performanti."      
+}
+
+export const copy = {
+    "official_Startupper_URL": "https://dominio.com",
+    "official_Startupper_title": "NomeAgenzia",
+    "official_Startupper_description": "Break official benefit entire. Person I morning have do. Place family child inside admit production. Rather movement like cell later. Bring six daughter get far full. Easy book Mr happy sit. Accept tree then old. Test natural deal window these pressure. Like describe write someone. Give thought set American.",
+    "official_Startupper_slogan": "Lorem ipsum super tiger mama bruta",
+    "header": {
+        "title": "",
+        "subtitle_short": "",
+        "call_to_action": cta_1
+    },
+    "banner": {
+        "cta_1": cta_1,
+        "cta_2": cta_2,
+        "call_to_action": cta_1,
+    },
+    "servizi": {
+        "title": "Le Nostre Soluzioni",
+        "subtitle": cta_2,
+    },
+    "tecnologie": {
+        "title": "Multi Ambiente",
+        "subtitle": "Le Nostre Tecnologie",
+    },
+    "footer": {
+        "title": "",
+        "subtitle": "",
+        "call_to_action": ""
+    },
+    "contact": {
+        "phone1": "3456789876",
+        "phone2": "",
+        "wa1": "3456789876",
+        "wa2": "",
+        "mail1": "ifo@nomeagenzia.com",
+        "mail2": "Iscriviti",
+    },
+    "chisiamo": {
+        "title": "Chi Siamo",
+        "subtitle": "Il Team di" + identity.name,
+    },
+    "footersubscribe": {
+        "title": cta_1,
+        "subtitle": "Iscriviti ora",
+        "subsubtitle": "Newsletter",
+        "body": "Inserisci la tua Email",
+        "button": "Invia",
+    },
+    "team": {
+        "antonio": {
+            "image": "/img/team/team-1.jpg",
+            "name": "Antonio Stoico",
+            "title": "CTO",
+            "social": {
+                "linkedin": {
+                  "url": "https://www.linkedin.com/in/antonio-stoico-820113/",
+                  "icon": "fab fa-linkedin",
+                },
+                "instagram": {
+                  "url": "",
+                  "icon": "",
+                },
+                "facebook": {
+                  "url": "",
+                  "icon": "",
+                },
+                "twitter": {
+                  "url": "",
+                  "icon": "",
+                },
+                "whatsapp": {
+                  "url": "",
+                  "icon": "",
+                },
+                "site": {
+                  "url": "",
+                  "icon": "",
+                },
+            },
+        },
+        "emanuele": {
+            "image": "/img/team/team-3.jpg",
+            "name": "Emanuele Mancari",
+            "title": "Software Engeenier",
+            "social": {
+                "linkedin": {
+                  "url": "https://www.linkedin.com/in/emanuele-mancari-b4b10171/",
+                  "icon": "fab fa-linkedin",
+                },
+                "instagram": {
+                  "url": "",
+                  "icon": "",
+                },
+                "facebook": {
+                  "url": "",
+                  "icon": "",
+                },
+                "twitter": {
+                  "url": "",
+                  "icon": "",
+                },
+                "whatsapp": {
+                  "url": "https://wa.me/" + emanuele_wa,
+                  "icon": "fab fa-whatsapp",
+                },
+                "site": {
+                  "url": "https://www.laparteweb.it/",
+                  "icon": "fab fa-safari",
+                },
+            },
+        }
+    },
+    "services": [
+        {
+            "number": "01",
+            "img": "/img/icons/feature-icon-3.png",
+            "title": identity.services[0],
+            "description": identity.servicesDescription[0],
+        },
+        {
+            "number": "02",
+            "img": "/img/icons/service-icon-1.png",
+            "title": identity.services[1],
+            "description": identity.servicesDescription[1],
+        },
+        {
+            "number": "03",
+            "img": "/img/icons/service-icon-2.png",
+            "title": identity.services[2],
+            "description": identity.servicesDescription[2],
+        },
+        {
+            "number": "04",
+            "img": "/img/icons/service-icon-3.png",
+            "title": identity.services[3],
+            "description": identity.servicesDescription[3],
+        },
+        {
+            "number": "05",
+            "img": "/img/icons/service-icon-5.png",
+            "title": identity.services[4],
+            "description": identity.servicesDescription[4],
+        },
+    ],
+};
+  
 const data = [
     {
         id:1,
