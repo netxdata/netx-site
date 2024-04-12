a4041d4a53b9ab54f56bbee9d34e09bd87c542e2
Emanuele Mancari <emanuele.mancari@abstract.it>
2024-04-12 08:59:21 +0200

    make content dynamic

--- src/components/common/SubscribArea.jsx
+++ src/components/common/SubscribArea.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { copy } from '../../data/data'
 
 function SubscribArea() {
   return (
@@ -9,15 +10,15 @@ function SubscribArea() {
             <div className="row align-items-center">
               <div className="col-lg-6 wow animate fadeInLeft" data-wow-delay="200ms" data-wow-duration="1500ms">
                 <div className="subscribe-cnt">
-                  <span>Get In Touch</span>
-                  <h3>Subscribe Our</h3>
-                  <strong>Newsletter</strong>
+                  <span>{copy.footersubscribe.title}</span>
+                  <h3>{copy.footersubscribe.subtitle}</h3>
+                  <strong>{copy.footersubscribe.subsubtitle}</strong>
                 </div>
               </div>
               <div className="col-lg-6 wow animate fadeInRight" data-wow-delay="200ms" data-wow-duration="1500ms">
                 <div className="subscribe-form">
                   <form onScroll={(e)=>e.preventDefault()} action="#" method="post">
-                    <input type="email" name="email" placeholder="Type Your Email" />
+                    <input type="email" name="email" placeholder={copy.footersubscribe.body} />
                     <input type="submit" defaultValue="connect" />
                   </form>
                 </div>
