717ff7e0495d950ca11f940a175588b8c7d097c6
Emanuele Mancari <emanuele.mancari@epress.local>
2024-05-06 11:06:25 +0200

    enlarge form note textarea

--- src/components/common/mailchimp/MailchimpFullForm.jsx
+++ src/components/common/mailchimp/MailchimpFullForm.jsx
@@ -14,7 +14,7 @@ function MailchimpFullForm() {
       <div id="mc_embed_signup_scroll">
           <div className="mc-field-group"><input type="email" name="EMAIL" className="required email" id="mce-EMAIL" placeholder={copy.contact.form.copy2} required /></div>
           <div className="mc-field-group"><input type="text" name="FNAME" className=" text" id="mce-FNAME" placeholder={copy.contact.form.copy1} /></div>
-          <div className="mc-field-group"><input type="text" name="MMERGE4" className=" text" id="mce-MMERGE4" placeholder={copy.contact.form.copy3} /></div>
+          <div className="mc-field-group"><textarea className=" text" id="mce-MMERGE4" name="MMERGE4" placeholder={copy.contact.form.copy3} rows="4" cols="50"></textarea></div>
           <div id="mce-responses" className="clear foot">
               <div className="response" id="mce-error-response" style={{ display: 'none' }}></div>
               <div className="response" id="mce-success-response" style={{ display: 'none' }}></div>
